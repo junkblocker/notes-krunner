8f25d9216a8c0a6fb52cb542146f34d11993c47c
Manpreet Singh <junkblocker@yahoo.com>
2023-06-17 16:02:59 -0700

    Attempt to open filenames with space DOES NOT WORK

--- %{APPNAMELC}.py
+++ %{APPNAMELC}.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """A Plasma runner."""
-# import q
 import os
 import subprocess
 from contextlib import suppress
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
+from urllib.parse import quote
 
 import dbus.service
+#import q
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
+
+# from urllib.parse import quote
+
 
 DBusGMainLoop(set_as_default=True)
 
@@ -18,66 +22,32 @@ IFACE = "org.kde.krunner1"
 SERVICE = "org.kde.%{APPNAMELC}"
 
 
-def get_openers(data: str, action_id: str) -> List[List[str]]:
-    openers: List[List[str]] = [[]]
+def get_opener(data: str) -> List[str] | None:
 
     (vault, note) = data.rsplit("|")
     datapath = str(Path(vault, note))
 
-    if (
-        Path("/var/lib/flatpak/app/md.obsidian.Obsidian").exists()
-        or Path(os.environ["HOME"] + "/Applications/Obsidian.AppImage").exists()
-    ):
+    if (Path("/var/lib/flatpak/app/md.obsidian.Obsidian").exists()
+            or Path(os.environ["HOME"] + "/Applications/Obsidian.AppImage").exists()):
         if Path(vault, note).exists():
-            openers = [
-                [
-                    "xdg-open",
-                    "obsidian://open?vault=notes&file=" + note.replace(" ", "%20"),
-                ]
+            return [
+                "xdg-open",
+                f"obsidian://open?vault=notes&file={quote(note)}",
             ]
-        else:
-            openers = [
-                [
-                    "xdg-open",
-                    "obsidian://new?vault=notes&file=" + note.replace(" ", "%20"),
-                ]
-            ]
-        return openers
+        return [
+            "xdg-open",
+            f"obsidian://new?vault=notes&file={quote(note)}",
+        ]
 
-    if Path(os.path.expanduser(os.path.expandvars("~/.config/prefer_nvim"))).exists():
-        if Path("/usr/bin/nvim-qt").exists():
-            if Path(os.environ["HOME"] + "/.local/bin/nvr").exists():
-                openers += [
-                    [os.environ["HOME"] + "/.local/bin/nvr", "--remote-tab", datapath]
-                ]
-            else:
-                openers += [["/usr/bin/nvim-qt", datapath]]
-        elif Path("/usr/bin/nvim").exists() and Path("/usr/bin/konsole").exists():
-            openers += [["/usr/bin/konsole", "-e", "/usr/bin/nvim", datapath]]
-    # elif Path("/usr/bin/gvim").exists():
-    #     openers += [["/usr/bin/gvim", "--remote-tab", datapath]]
-    elif Path("/usr/bin/nvim").exists():
-        if Path(os.environ["HOME"] + "/.local/bin/nvr").exists():
-            openers += [
-                [
-                    "/usr/bin/konsole",
-                    "-e",
-                    os.environ["HOME"] + "/.local/bin/nvr",
-                    "--remote-tab",
-                    datapath,
-                ]
-            ]
-        else:
-            openers += [["/usr/bin/konsole", "-e", "/usr/bin/nvim", datapath]]
-    elif Path("/usr/bin/vim").exists():
-        openers += [["/usr/bin/konsole", "-e", "/usr/bin/vim", datapath]]
-    elif Path("/usr/bin/kate").exists():
-        openers += [["/usr/bin/kate", datapath]]
-    elif Path("/usr/bin/kwrite").exists():
-        openers += [["/usr/bin/kwrite", datapath]]
-    else:
-        openers += [["/usr/bin/gedit", datapath]]
-    return openers
+    for opt in ('/usr/bin/nvim-qt', '/usr/bin/kate', '/usr/bin/kwrite', '/usr/bin/gedit'):
+        if Path(opt).exists():
+            return [opt, datapath]
+
+    for opt in ('/usr/bin/nvim', '/usr/bin/vim', '/usr/bin/nano'):
+        if Path(opt).exists():
+            return ["/usr/bin/konsole", "-e", opt, datapath]
+
+    return None
 
 
 class Runner(dbus.service.Object):
@@ -96,11 +66,12 @@ class Runner(dbus.service.Object):
     @dbus.service.method(IFACE, in_signature="s", out_signature="a(sssida{sv})")
     def Match(self, query: str):
         """This method is used to get the matches and it returns a list of tuples"""
-        # TODO: NoMatch = 0, CompletionMatch = 10, PossibleMatch = 30, InformationalMatch = 50, HelperMatch = 70, ExactMatch = 100
+        # NoMatch = 0, CompletionMatch = 10, PossibleMatch = 30, InformationalMatch = 50, HelperMatch = 70, ExactMatch = 100
 
         # Tried to use results as a dict itself but the {'subtext': line} portion is not hashable :/
         seen: Dict[str, float] = {}
-        results = []
+
+        results: List[Tuple[str, str, str, int, float, Dict[str, str]]] = []
 
         pwd = Path.cwd()
         found = False
@@ -127,15 +98,11 @@ class Runner(dbus.service.Object):
             result = subprocess.run(expr, capture_output=True, check=False)
             for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                 if line != "" and ".obsidian/" not in line:
-                    if lcquery == create.lower() and (
-                        (line not in seen) or seen[line] < 1.0
-                    ):
+                    if lcquery == create.lower() and ((line not in seen) or seen[line] < 1.0):
                         seen[f"{ndir}|{line}"] = 1.0
                         found = True
                         continue
-                    if lcquery in line.lower() and (
-                        (line not in seen) or seen[line] < 1.0
-                    ):
+                    if lcquery in line.lower() and ((line not in seen) or seen[line] < 1.0):
                         seen[f"{ndir}|{line}"] = 0.97
                         found = True
                         continue
@@ -158,9 +125,7 @@ class Runner(dbus.service.Object):
             result = subprocess.run(expr, capture_output=True, check=False)
             for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                 if line != "" and ".obsidian/" not in line:
-                    if lcquery in line.lower() and (
-                        (line not in seen) or (seen[line] < 0.95)
-                    ):
+                    if lcquery in line.lower() and ((line not in seen) or (seen[line] < 0.95)):
                         seen[f"{ndir}|{line}"] = 0.95
                         found = True
                         continue
@@ -185,9 +150,7 @@ class Runner(dbus.service.Object):
             result = subprocess.run(expr, capture_output=True, check=False)
             for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                 if line != "" and ".obsidian/" not in line:
-                    if lcquery in line.lower() and (
-                        (line not in seen) or (seen[line] < 0.90)
-                    ):
+                    if lcquery in line.lower() and ((line not in seen) or (seen[line] < 0.90)):
                         seen[f"{ndir}|{line}"] = 0.90
                         found = True
                         continue
@@ -212,9 +175,7 @@ class Runner(dbus.service.Object):
             result = subprocess.run(expr, capture_output=True, check=False)
             for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                 if line != "" and ".obsidian/" not in line:
-                    if lcquery in line.lower() and (
-                        (line not in seen) or (seen[line] < 0.85)
-                    ):
+                    if lcquery in line.lower() and ((line not in seen) or (seen[line] < 0.85)):
                         seen[f"{ndir}|{line}"] = 0.85
                         found = True
                         continue
@@ -239,9 +200,7 @@ class Runner(dbus.service.Object):
             result = subprocess.run(expr, capture_output=True, check=False)
             for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                 if line != "" and ".obsidian/" not in line:
-                    if lcquery in line.lower() and (
-                        (line not in seen) or (seen[line] < 0.80)
-                    ):
+                    if lcquery in line.lower() and ((line not in seen) or (seen[line] < 0.80)):
                         seen[f"{ndir}|{line}"] = 0.80
                         found = True
                         continue
@@ -263,9 +222,7 @@ class Runner(dbus.service.Object):
                 result = subprocess.run(expr, capture_output=True, check=False)
                 for line in str.split(result.stdout.decode("UTF-8"), "\n"):
                     if line != "" and ".obsidian/" not in line:
-                        if lcquery in line.lower() and (
-                            (line not in seen) or (seen[line] < 0.75)
-                        ):
+                        if lcquery in line.lower() and ((line not in seen) or (seen[line] < 0.75)):
                             seen[f"{ndir}|{line}"] = 0.75
                             continue
                         if line not in seen:
@@ -277,32 +234,32 @@ class Runner(dbus.service.Object):
             if ".stversions/" in item:
                 score = score - 0.10
             # data, text, icon, type (Plasma::QueryType), relevance (0-1), properties (subtext, category and urls)
-            results += [
-                (
-                    item,
-                    item.rsplit("|")[-1],
-                    "document-edit",
-                    int(score * 100),
-                    score,
-                    {"subtext": item},
-                )
-            ]
+            results += [(
+                item,
+                item.rsplit("|")[-1],
+                "document-edit",
+                int(score * 100),
+                score,
+                {
+                    "subtext": item
+                },
+            )]
 
         # If a file with exact match was not found, provide a creation option
         has_file = any(line.lower().endswith(f"/{create.lower()}") for line in seen)
         if not has_file:
             for ndir in self.notes_dirs:
                 create = f"{ndir}|{query}.md"
-                results += [
-                    (
-                        f"{create}",
-                        f"Create {ndir}/{query}.md",
-                        "document-edit",
-                        85,
-                        1.0,
-                        {"subtext": create},
-                    )
-                ]
+                results += [(
+                    f"{create}",
+                    f"Create {ndir}/{query}.md",
+                    "document-edit",
+                    85,
+                    1.0,
+                    {
+                        "subtext": create
+                    },
+                )]
 
         return results
 
@@ -314,24 +271,10 @@ class Runner(dbus.service.Object):
 
     @dbus.service.method(IFACE, in_signature="ss")
     def Run(self, data: str, action_id: str):
-        wraise = Path("~/bin/wraise").expanduser()
-        openers = get_openers(data, action_id)
-        for opener in openers:
-            with suppress(Exception):
+        with suppress(Exception):
+            opener = get_opener(data)
+            if opener:
                 _ = subprocess.Popen(opener).pid
-                if wraise.exists():
-                    _ = subprocess.Popen([wraise, "-f", "gvim"]).pid
-                elif os.environ["XDG_SESSION_TYPE"] == "x11":
-                    result = subprocess.run(
-                        ["/usr/bin/xdotool", "search", "--classname", "gvim"],
-                        check=False,
-                        capture_output=True,
-                    )
-                    for line in str.split(result.stdout.decode("UTF-8"), "\n"):
-                        if line != "":
-                            _ = subprocess.Popen(
-                                ["xdotool", "windowactivate", line]
-                            ).pid
 
 
 # print(data, action_id)
